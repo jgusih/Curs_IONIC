5d58ac75be7ef82a0be11a132498e6e29746541f
DESKTOP-A8GGULU\jgusi <jgusi@hotmail.com>
2018-07-31 20:37:53 +0200

    sonidos S3 C30

--- 02-sonidos/src/pages/home/home.ts
+++ 02-sonidos/src/pages/home/home.ts
@@ -10,6 +10,8 @@ import { Animal } from "../../interfaces/animal.interface";
 export class HomePage {
 
   animales:any[]=[];
+  audio = new Audio();
+  audioTiempo: any;
 
   constructor() {
 
@@ -18,17 +20,41 @@ export class HomePage {
   }
 
   reproducir (animal:Animal){
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo){
+      animal.reproduciendo = false;
+      return;
+    }
+
     console.log(animal);
 
-    let audio = new Audio();
-    audio.src=animal.audio;
 
-    audio.load();
-    audio.play();
+    this.audio.src=animal.audio;
+
+    this.audio.load();
+    this.audio.play();
 
     animal.reproduciendo = true;
 
-    setTimeout( () => animal.reproduciendo = false, animal.duracion = 1000);
+    this.audioTiempo = setTimeout( () => animal.reproduciendo = false, animal.duracion = 1000);
+  }
+
+  private pausar_audio ( animalSel: Animal){
+
+    clearTimeout ( this.audioTiempo );
+
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for ( let animal of this.animales){
+
+      if (animal.nombre != animalSel.nombre){
+        animal.reproduciendo = false;
+      }
+
+    }
+
   }
 
 }
